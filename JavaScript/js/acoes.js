9cf88716361fade96dc4cf9fafd28270d5d2c07d
cnventura <camila.nventura@senacsp.edu.br>
2024-03-14 17:28:24 -0300

    feito

---
+++ JavaScript/js/acoes.js
@@ -0,0 +1,4 @@
+//alert('Funcionou');
+'use strict'
+
+document.getElementById('caixa').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo porro, modi numquam vero a quo consequuntur aperiam. Earum vel amet placeat sunt adipisci et cumque deleniti autem maiores vitae?'
---
+++ JavaScript/js/acoesCorSignificado.js
@@ -0,0 +1,4 @@
+//alert('Funcionou');
+'use strict'
+
+ onclick= document.getElementById('azul').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo porro, modi numquam vero a quo consequuntur aperiam. Earum vel amet placeat sunt adipisci et cumque deleniti autem maiores vitae?'
