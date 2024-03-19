d59bab6179dab49d4fe2f2d2f9af1c9553643d60
cnventura <camila.nventura@senacsp.edu.br>
2024-03-18 23:05:10 -0300

    feito

--- JavaScript/js/acoesCorSignificado.js
+++ JavaScript/js/acoesCorSignificado.js
@@ -1,9 +1,9 @@
 alert('Funcionou');
 'use strict'
 
-//onclick="document.getElementById('Azul').style.display='caixa'"
+onclick="document.getElementById('Azul').style.display='caixa'"
 
 
 
 
-//document.getElementById('caixa').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo porro, modi numquam vero a quo consequuntur aperiam. Earum vel amet placeat sunt adipisci et cumque deleniti autem maiores vitae?'
+document.getElementById('caixa').innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia illo porro, modi numquam vero a quo consequuntur aperiam. Earum vel amet placeat sunt adipisci et cumque deleniti autem maiores vitae?'
